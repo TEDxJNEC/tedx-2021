af39c135edb465e2e3bac328d363f53bf084cc00
ayushnayyar <ayushnayyar48@gmail.com>
2021-01-19 18:38:15 +0530

    Added Content Page
    Added Content Page to components directory (with dummy data), modified main.scss to accomodate styles for the Content directory and referenced the Content component in App.jsx.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import logo from "./logo.svg";
-import "./App.scss";
+import logo from './logo.svg';
+import './App.scss';
+import Content from './components/content';
 
 function App() {
-	return (
-		<div className='App'>
-			<header className='App-header'>
-				<img src={logo} className='App-logo' alt='logo' />
-				<h1 className={"App-headline"}>KINTSUGI</h1>
-			</header>
-		</div>
-	);
+    return (
+        <div className='App'>
+            <header className='App-header'>
+                <img src={logo} className='App-logo' alt='logo' />
+                <h1 className={'App-headline'}>KINTSUGI</h1>
+            </header>
+            <Content />
+        </div>
+    );
 }
 
 export default App;
