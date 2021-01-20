ddd96459b417e4c364ba99005ae1f8fbc2802f1a
Soham Tamboli <sohamtamboli@gmail.com>
2021-01-20 15:57:28 +0530

    Routing Initialised
    Added
    - react-router-dom
    - routes folder
    - index page/ home page

---
+++ src/pages/index.jsx
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.scss";
+
+function Home() {
+	return (
+		<div className='App'>
+			<header className='App-header'>
+				<img src={logo} className='App-logo' alt='logo' />
+				<h1 className={"App-headline"}>KINTSUGI</h1>
+			</header>
+		</div>
+	);
+}
+
+export default Home;
