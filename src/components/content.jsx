69e475df61b09ca3388fab99ed4d61dd5b8f33c8
ayushnayyar <ayushnayyar48@gmail.com>
2021-01-22 02:11:20 +0530

    Added Content Component to About Page and added some global CSS classes
    - Added about-content.json to assets directory
    - Made the Content component reusable, by accepting props (Content.jsx)
    - Added Content Component to About Page and supplied relevant pro (about.jsx)
    -Added few CSS classes for global styles in main.scss

--- src/components/content.jsx
+++ src/components/content.jsx
@@ -1,33 +1,21 @@
 import React from 'react';
 import '../common/main.scss';
 
-const Content = () => {
+const Content = ({ headline, content, iframeLink, iFrameTitle }) => {
     return (
         <React.Fragment>
             <section>
                 <div className='row'>
                     <div className='column'>
-                        <h1 className='content-headline'>Test Headline</h1>
-                        <p>
-                            In the spirit of ideas worth spreading, TED has
-                            created a program called TEDx. TEDx is a program of
-                            local, self-organized events that bring people
-                            together to share a TED-like experience. Our event
-                            is called TEDxJNEC, where x = independently
-                            organized TED event. At our TEDxJNEC event, TEDTalks
-                            video and live speakers will combine to spark deep
-                            discussion and connection in a small group. The TED
-                            Conference provides general guidance for the TEDx
-                            program, but individual TEDx events, including ours,
-                            are self-organized.
-                        </p>
+                        <h1 className='content-headline'>{headline}</h1>
+                        <p>{content}</p>
                     </div>
                     <div className='column'>
                         <div className='content-wrapper'>
                             <iframe
                                 className='tedx-iframe'
-                                title='TEDx'
-                                src='https://www.youtube.com/embed/d0NHOpeczUU'
+                                title={iFrameTitle}
+                                src={iframeLink}
                                 frameborder='0'
                                 allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
                                 allowfullscreen
--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,7 +1,22 @@
-import React from "react";
+import React from 'react';
+import aboutPageContent from '../assets/about-content.json';
+import Content from '../components/Content';
 
 const About = () => {
-	return <div></div>;
+    const content = aboutPageContent.map((aboutContent) => {
+        return (
+            <div>
+                <Content
+                    headline={aboutContent.headline}
+                    content={aboutContent.content}
+                    iframeTitle={aboutContent.tedxIframeTitle}
+                    iframeLink={aboutContent.tedxIframeLink}
+                />
+            </div>
+        );
+    });
+
+    return <div>{content}</div>;
 };
 
 export default About;
