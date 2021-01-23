49bcb34bea196d4f34e4a963b3e631f76774ecb4
Pruthvi <pruthvishetty5656@gmail.com>
2021-01-23 17:12:35 +0530

    fixed errors

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,7 +1,6 @@
-import logo from '../logo.svg';
+import logo from '../assets/logo.svg';
 import "../common/main.scss";
 import DefaultLayout from '../layouts'
-import Footer from '../components/Footer';
 function Home() {
     return (
         <DefaultLayout>
