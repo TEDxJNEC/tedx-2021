95b2b7088d4cf7820dd01b4e70afbfd9661e1dbf
Soham Tamboli <sohamtamboli@gmail.com>
2021-01-25 19:55:12 +0530

    Added Team Route
    committee wise team route

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,15 +3,17 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { ROUTES } from "../constants/routes";
 import Home from "../pages/index";
 import About from "../pages/about";
+import Team from "../pages/team";
 
 const Routes = () => {
-	const { HOME, ABOUT } = ROUTES;
+	const { HOME, ABOUT, TEAM } = ROUTES;
 	return (
 		<Router basename='/tedx-2021'>
 			<Switch>
 				<Route exact path={HOME} component={Home}></Route>
 				<Route exact path={ABOUT} component={About}></Route>
-				{/* <Route exact path={TEAM} component={}></Route> */}
+				<Route exact path={TEAM} component={Team}></Route>
+				<Route exact path={`${TEAM}/:committee`}></Route>
 				{/* <Route exact path={BOOKINGS} component={}></Route> */}
 			</Switch>
 		</Router>
