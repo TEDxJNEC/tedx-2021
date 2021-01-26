8ae217cb118e3d02377ae19b4390e6e5518e217b
Riya Jain <66663462+riaaajain@users.noreply.github.com>
2021-01-26 19:48:58 +0530

    Footer code
    All icons are present in components where the icons folder is present.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,15 +1,38 @@
-import React from 'react';
-
- const Footer = () =>{
-     
-        return (
+import "../common/main.scss";
+import {Facebook,Insta} from "../components/icons";
+const Footer = () => {
+    return (
         <div>
-            <h1 className='footerClass'>Footer</h1>
+            {/* <h1 className="footerClass">Footer</h1> */}
+            <footer>
+                <div className="main-content">
+                    <div className="left">
+                        <h2>TEDx JNEC</h2>
+                        <div className="content">
+                            <p>TEDxJNEC is independently
+                            organized TED event. Officially
+                            licensed by TED.</p>
+                            <div className="social">
+                                <a> <Facebook /></a>
+                                <a><Insta/></a>
+                                <a><span className=""></span></a>
+                                <a><span className=""></span></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="center box">
+                        <h2>CONTACT</h2>
+                        <div className="content">
+                            <div className="place">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+
         </div>
-        )
-    }
- 
+    )
+}
 
 export default Footer
 
- 
