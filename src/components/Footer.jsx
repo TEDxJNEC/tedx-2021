57e199a79cb98d962de632be0210ffa77fae00a2
Riya Jain <66663462+riaaajain@users.noreply.github.com>
2021-01-27 21:17:23 +0530

    Footer code
    Footer code
    - code
    - all icons are added in components page

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,32 +1,49 @@
 import "../common/main.scss";
-import {Facebook,Insta} from "../components/icons";
+import "../common/footer.scss";
+import {Logo,Facebook,Insta,Twitter,Youtube,Linkedin,Home,Email,Phone} from "../components/icons";
 const Footer = () => {
     return (
         <div>
             {/* <h1 className="footerClass">Footer</h1> */}
-            <footer>
+            <footer className="footer">
                 <div className="main-content">
-                    <div className="left">
-                        <h2>TEDx JNEC</h2>
+                    <div className="left box">
+                        <h2> <Logo/></h2>
                         <div className="content">
-                            <p>TEDxJNEC is independently
-                            organized TED event. Officially
-                            licensed by TED.</p>
+                            <p>TEDx JNEC is independently<br/>
+                            organized TED event. Officially<br/>
+                            licensed by TED.</p><br/>
                             <div className="social">
-                                <a> <Facebook /></a>
-                                <a><Insta/></a>
-                                <a><span className=""></span></a>
-                                <a><span className=""></span></a>
+                                <a> <span><Facebook/> </span></a>
+                                <a> <span><Twitter/> </span> </a>
+                                <a><span><Youtube/> </span></a>
+                                <a><span><Insta/> </span></a>
+                                <a><span><Linkedin/> </span></a>
+
                             </div>
                         </div>
                     </div>
-                    <div className="center box">
+                    <div className="right box">
                         <h2>CONTACT</h2>
                         <div className="content">
                             <div className="place">
+                                <a><span className="icon">< Home/> </span></a>
+                                <span className="text">MGM's Jawaharlal Nehru
+                                Engineering College, Aurangabad.</span>
+                            </div>
+                            <div className="email">
+                                <a> <span className="icon">< Email/> </span> </a>
+                                <span className="text">contact@tedxjnec.com </span>
+                            </div>
+                            <div className="phone">
+                                <a><span className="icon">< Phone/> </span> </a>
+                                <span className="text">7218078452   </span>
                             </div>
                         </div>
                     </div>
+                </div>
+                <div className="foot">
+                    <p>Made By WebDevelopment Team : TEDxJNEC 2021</p>
                 </div>
             </footer>
 
