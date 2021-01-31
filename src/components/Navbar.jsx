d1cd4716b331c9f517f388eeb21bb591884d875f
Shrikant Pandhekar <shrikantp9762@gmail.com>
2021-01-31 19:24:55 +0530

    Change in Code for navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,34 +2,42 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import { ROUTES } from "../constants/routes";
 import "../common/nav.scss";
-import img from "../assets/tedxjneclogo_white.svg";
+import img from "../assets/JNEC.svg";
 import { MenuIcon } from "./Icons/MenuIcon";
 
 const Navbar = () => {
   const { HOME, ABOUT } = ROUTES;
-  return (
+  // state = {
+  //   toggle:false
+  // }
+   const handleClick = () => {
+  //   this.setState(toggle:!this.state.toggle)
+     
+   }
+   return (
     <>
+    
       <nav>
         <div className="logo">
           <img src={img} className="logo_tedx" alt="TEDxJNEC_logo" />
         </div>
-        <div className="menu_logo">
+        <div className="menu_logo" onClick={handleClick}>
           <MenuIcon />
         </div>
         <ul className="links">
-          <NavLink to={HOME} exact activeClassName className="title">
+          <NavLink to={HOME} exact activeClassName="active" className="title">
             HOME
           </NavLink>
-          <NavLink to="/" exact activeClassName className="title">
+          <NavLink to="gy" exact activeClassName="active" className="title">
             SPEAKERS
           </NavLink>
-          <NavLink to={ABOUT} exact activeClassName className="title">
+          <NavLink to={ABOUT} exact activeClassName="active" className="title">
             ABOUT
           </NavLink>
-          <NavLink to="/" exact activeClassName className="title">
+          <NavLink to="gfsdh" exact activeClassName="active" className="title">
             BOOKING
           </NavLink>
-          <NavLink to="/" exact activeClassName className="title">
+          <NavLink to="xc" exact activeClassName="active" className="title">
             TEAM
           </NavLink>
         </ul>
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,14 +2,14 @@ import logo from '../logo.svg';
 import '../App.scss';
 import Footer from '../components/Footer';
 import Navbar from '../components/Navbar';
+import Menu from "../components/Menu";
 
 function Home() {
     return (
         <>
-        <Navbar />
+        <Menu />
         <div className='App'>
-            
-            
+ 
                 <img src={logo} className='App-logo' alt='logo' />
                 <h1 className={'App-headline'}>KINTSUGI</h1>
            
