8fba23f970e635fb34c8ae019effd9c58d87098f
Shrikant Pandhekar <shrikantp9762@gmail.com>
2021-02-01 21:06:48 +0530

    Completed Navbar
    - Solved all issues

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,148 +1,100 @@
-import React, { useState, useEffect } from "react";
+import React, { useState} from "react";
 import { NavLink } from "react-router-dom";
 import { ROUTES } from "../constants/routes";
 import "../common/nav.scss";
 import img from "../assets/JNEC.svg";
 import { MenuIcon } from "./icons/MenuIcon";
+import { Closemenu } from "./icons/Closemenu";
 const { HOME, ABOUT } = ROUTES;
 
-const OpenList = () => {
-
-  return(
-      <>
-      
-      <div className="mobile_menu">
-      <ul className="mobi_links">
-        <NavLink to={HOME} exact activeClassName="active" className="mobi_title">
-          HOME
-        </NavLink>
-        <NavLink to="gy" exact activeClassName="active" className="mobi_title">
-          SPEAKERS
-        </NavLink>
-        <NavLink to={ABOUT} exact activeClassName="active" className="mobi_title">
-          ABOUT
-        </NavLink>
-        <NavLink to="gfsdh" exact activeClassName="active" className="mobi_title">
-          BOOKING
-        </NavLink>
-        <NavLink to="xc" exact activeClassName="active" className="mobi_title">
-          TEAM
-        </NavLink>
-      </ul>
-  </div>
-      </>
-  )
-}
 
 const Desktop = () => {
     const { HOME, ABOUT } = ROUTES;
-    const [isMobile,setMobile] =  useState(
-      false
-    );
-    useEffect(() => { 
-       // window.addEventListener("resize",() => {
-       //   setMobile(window.matchMedia("(max-width:759px)").matches);  
-       // });
-       if (window.screen.width <= 760) {
-         setMobile(true)
-         
-       }
-    }, []);
-    const [activeMobile,setActiveMobile] = useState(false)
-     
-    const Toggle = () => {
-      setActiveMobile(!activeMobile);
-      
-  
-    };
     return(
         <>
          <nav>
         <div className="logo">
           <img src={img} className="logo_tedx" alt="TEDxJNEC_logo" />
         </div>
-        {isMobile ? <> <div className="menu_logo" onClick={Toggle}> <MenuIcon /> </div></>: <ul className="links">
+        <ul className="links">
           <NavLink to={HOME} exact activeClassName="active" className="title">
             HOME
           </NavLink>
-          <NavLink to="gy" exact activeClassName="active" className="title">
+          <NavLink to={"/speakers"} exact activeClassName="active" className="title">
             SPEAKERS
           </NavLink>
           <NavLink to={ABOUT} exact activeClassName="active" className="title">
             ABOUT
           </NavLink>
-          <NavLink to="gfsdh" exact activeClassName="active" className="title">
+          <NavLink to={"/booking"} exact activeClassName="active" className="title">
             BOOKING
           </NavLink>
-          <NavLink to="xc" exact activeClassName="active" className="title">
+          <NavLink to={"/team"} exact activeClassName="active" className="title">
             TEAM
           </NavLink>
-        </ul>}
+        </ul>
       </nav>
         </>
     )
 }
  
 const Mobile = () => {
-    const [activeMobile,setActiveMobile] = useState(false)
-     
+    const [active,setActive] = useState(false)
+    const [navClass,setNavClass]=useState("mobile_menu")
     const Toggle = () => {
-      setActiveMobile(!activeMobile);
-  
+      setNavClass(active? "mobile_menu outanimation" :'mobile_menu inanimation')
+      !active?document.body.classList.add('roll'):document.body.classList.remove('roll')
+      setActive(!active);
+     
     };
+ 
+  
     return (
         <>
     <nav>
-    {/* <div className="logo">
+    <div className="logo">
       <img src={img} className="logo_tedx" alt="TEDxJNEC_logo" />
-    </div> */}
-    <div className="menu_logo" onClick={Toggle}>
-      Close
     </div>
-{activeMobile ? (<div className="mobile_menu">
-<ul className="mobi_links">
+    
+
+
+{active ? (<> 
+  <div className="menu_logo" onClick={Toggle}>
+      <Closemenu />
+    </div></>):    <div className="menu_logo" onClick={Toggle}>
+      <MenuIcon />
+    </div>}
+    <div className={navClass}>
+        <ul className="mobi_links">
           <NavLink to={HOME} exact activeClassName="active" className="mobi_title">
             HOME
           </NavLink>
-          <NavLink to="gy" exact activeClassName="active" className="mobi_title">
+          <NavLink to={"/speakers"} exact activeClassName="active" className="mobi_title">
             SPEAKERS
           </NavLink>
           <NavLink to={ABOUT} exact activeClassName="active" className="mobi_title">
             ABOUT
           </NavLink>
-          <NavLink to="gfsdh" exact activeClassName="active" className="mobi_title">
+          <NavLink to={"/booking"} exact activeClassName="active" className="mobi_title">
             BOOKING
           </NavLink>
-          <NavLink to="xc" exact activeClassName="active" className="mobi_title">
+          <NavLink to={"/team"} exact activeClassName="active" className="mobi_title">
             TEAM
           </NavLink>
         </ul>
-    </div>):null}
-    
+    </div>
     </nav>
     </>
     )
 }
 
 const Navbar = () => {
-  
-  // const [isMobile,setMobile] =  useState(
-  //    false
-  // );
-  // useEffect(() => { 
-  //     // window.addEventListener("resize",() => {
-  //     //   setMobile(window.matchMedia("(max-width:759px)").matches);  
-  //     // });
-  //     if (window.screen.width <= 760) {
-  //       setMobile(true)
-        
-  //     }
-  // });
+  const [isMobile,setMobile] =  useState(window.screen.width<720);
+
   
    return (
     <> 
-     {/* {isMobile ? <Mobile /> : <Desktop />} */}
-     <Desktop />
+     {isMobile ? <Mobile />:<Desktop />}
     </>
   );
 };
