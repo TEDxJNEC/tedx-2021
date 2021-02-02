1e5de3972a976730061da19022f489d91e99602d
Riya Jain <66663462+riaaajain@users.noreply.github.com>
2021-02-02 21:43:18 +0530

    highlights code(WIP)
    video link issue to be solved

---
+++ src/assets/highlights.js
@@ -0,0 +1,30 @@
+export const data = {
+    // drive links in square brackets
+    images2019: [
+        "",
+        "",
+    ], 
+    images2018: [
+        "",
+        "",
+        "",
+    ],
+    youtubeLinks19: [
+        "https://www.youtube.com/embed/8qyXirAeIOU",
+        "https://www.youtube.com/embed/le6eNngljto",
+        
+
+    ],
+    youtubeLinks18: [
+        "",
+
+    ],
+    
+}
+
+export const titles = {
+    label1: "GLIMPSE OF 2019",
+    label2: "GLIMPSE OF 2018",
+    iframeTitle1: "TEDx2019",
+    iframeTitle2: "TEDx2018",
+}
--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -1,35 +1,72 @@
 import React from 'react'
 import "../common/highlights.scss"
+import {data, titles} from "../assets/highlights.js"
 
-const images = [
-   "https://images.unsplash.com/photo-1558981359-219d6364c9c8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2100&q=80",
-   "https://images.unsplash.com/photo-1583585635793-0e1894c169bd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=913&q=80",
-   "https://images.unsplash.com/photo-1583531172005-814191b8b6c0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=975&q=80",
-   "https://images.unsplash.com/photo-1583426573939-97d09302d76a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=968&q=80",
-   "https://images.unsplash.com/photo-1583532452513-a02186582ccd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
-   "https://images.unsplash.com/photo-1583445013765-46c20c4a6772?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
-   "https://images.unsplash.com/photo-1583562835057-a62d1beffbf3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=949&q=80",
-   "https://images.unsplash.com/photo-1583562835057-a62d1beffbf3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=949&q=80",
-];
+
 
 
 const Highlights = () => {
     return (
         <div>
             <div className="container">
-                <div className="photo-grid">
-                    {images.map(src => (
-                        <div className="card"
-                            key={src}
-                            style={{
-                               
-                                backgroundImage: `url(${src})`
-                            }}
-                        />
+                <div>
+                    <h1>{titles.label1}</h1>
+                    <div className="photo-grid">
+                        {data.images2019.map(src => (
+                            <div className="card"
+                                key={src}
+                                style={{
+                                    backgroundImage: `url(${src})`
+                                }}
+                            />
+                            
+                        ))}
                         
-                    ))}
+                    </div>
+                    <div className="video">
+                        {data.youtubeLinks19.map(ytLink => (
+                            <iframe
+                            className='tedx-iframe'
+                            key={ytLink}
+                            title={titles.iFrameTitle1}
+                            src={ytLink}
+                            frameBorder='0'
+                            allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
+                            allowFullScreen
+                        ></iframe>
+                        ))}
+                        
+                    </div>
                 </div>
-                
+                <div>
+                    <h1>{titles.label2}</h1>
+                    <div className="photo-grid">
+                        {data.images2018.map(src => (
+                            <div className="card"
+                                key={src}
+                                style={{
+                                
+                                    backgroundImage: `url(${src})`
+                                }}
+                            />
+                            
+                        ))}
+                    </div>
+                    <div className="video">
+                        {data.youtubeLinks18.map(ytLink => (
+                            <iframe
+                            className='tedx-iframe'
+                            key={ytLink}
+                            title={titles.iFrameTitle2}
+                            src={ytLink}
+                            frameBorder='0'
+                            allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
+                            allowFullScreen
+                        ></iframe>
+                        ))}
+                        
+                    </div>
+                </div>
             </div>
         </div>
     )
