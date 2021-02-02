dc790643d84c3069e4c7fb06ee05e8e6944d9c75
Pruthvi <pruthvishetty5656@gmail.com>
2021-02-03 01:43:52 +0530

    Update index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -10,6 +10,7 @@ function Home() {
         <img src={logo} className="App-logo" alt="logo" />
         <h1 className="App-headline">KINTSUGI</h1>
       </div>
+      <div>Gell</div>
     </DefaultLayout>
   );
 }
