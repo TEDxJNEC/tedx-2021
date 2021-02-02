b995d506045a6540a54c8c81a65711daced4a657
Pruthvi <pruthvishetty5656@gmail.com>
2021-02-02 23:31:49 +0530

    Temporary Changes [REVERT INDEX.JSX] Incase something goes wrong

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -1,19 +1,14 @@
 import React from 'react'
 import "../common/highlights.scss"
-import {data, titles} from "../assets/highlights.js"
 
-
-
-
-const Highlights = () => {
+const Highlights = ({label,images,youtubeVideos}) => {
     return (
-        <div>
             <div className="container">
                 <div>
-                    <h1>{titles.label1}</h1>
+                    <h1>{label}</h1>
                     
                     <div className="photo-grid">
-                        {data.images2019.map(src => (
+                        {images.map(src => (
                             <div className="card"
                                 key={src}
                                 style={{
@@ -23,13 +18,13 @@ const Highlights = () => {
                             
                         ))}
                                             <div className="video">
-                        {data.youtubeLinks19.map(ytLink => (
+                        {youtubeVideos.map((ytVideo,index) => (
                             <div className="single-video">
                             <iframe
                             className='tedx-iframe'
-                            key={ytLink}
-                            title={titles.iFrameTitle1}
-                            src={ytLink}
+                            key={index}
+                            title={ytVideo.title}
+                            src={ytVideo.link}
                             frameBorder='0'
                             allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
                             allowFullScreen
@@ -41,39 +36,7 @@ const Highlights = () => {
                     </div>
 
                 </div>
-                <div>
-                    <h1>{titles.label2}</h1>
-                    <div className="photo-grid">
-                        {data.images2018.map(src => (
-                            <div className="card"
-                                key={src}
-                                style={{
-                                
-                                    backgroundImage: `url(${src})`
-                                }}
-                            />
-                            
-                        ))}
-                    </div>
-                    <div className="video">
-                        {data.youtubeLinks18.map(ytLink => (
-                            <div className="single-video">
-                            <iframe
-                            className='tedx-iframe'
-                            key={ytLink}
-                            title={titles.iFrameTitle2}
-                            src={ytLink}
-                            frameBorder='0'
-                            allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
-                            allowFullScreen
-                        ></iframe>
-                        </div>
-                        ))}
-                        
-                    </div>
-                </div>
             </div>
-        </div>
     )
 }
 
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,13 +1,33 @@
 import logo from '../assets/logo.svg';
 import "../common/main.scss";
 import DefaultLayout from '../layouts'
+// DELETE AFTER TESTING
+import Highlights from "components/Highlights"
+import highlightData from "assets/highlights.json"
+
+//END DELETE
 function Home() {
+    // DELETE AFTER TESTING
+    const renderHighlightData =()=>{
+        return highlightData.map((data)=>{
+            const {label,images,youtubeVideos}=data;
+            return (
+                <Highlights label={label} images={images} youtubeVideos={youtubeVideos} />
+            )
+        })
+    }
+    //END DELETE
     return (
         <DefaultLayout>
         <div className='App'>
                 <img src={logo} className='App-logo' alt='logo' />
                 <h1 className='App-headline'>KINTSUGI</h1>
         </div>
+        {/* DELETE AFTER TESTING */}
+        <div>
+          {renderHighlightData()}
+        </div>
+        {/* END DELETE */}
         </DefaultLayout>
     );
 }
