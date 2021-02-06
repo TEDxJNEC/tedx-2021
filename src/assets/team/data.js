8308429d8968c1912b6e332c4a860739902218e3
Soham Tamboli <sohamtamboli@gmail.com>
2021-02-06 14:49:49 +0530

    initial commit of committee page
    Added reusable component

---
+++ src/assets/team/data.js
@@ -0,0 +1,55 @@
+import webPruthvi from 'assets/team/web/soham.webp';
+import webSoham from 'assets/team/web/soham.webp';
+import webHuzaif from 'assets/team/web/soham.webp';
+import webAyush from 'assets/team/web/soham.webp';
+import webShrikant from 'assets/team/web/soham.webp';
+import webRiya from 'assets/team/web/soham.webp';
+
+const teamData = {
+  web: [
+    {
+      name: 'Pruthvi Shetty',
+      quirk1: 'Chill',
+      quirk2: 'Dependable',
+      img: webPruthvi,
+      quote: 'Watashi ga Kitta!',
+    },
+    {
+      name: 'Soham Tamboli',
+      quirk1: 'Cool',
+      quirk2: 'Dank',
+      img: webSoham,
+      quote: 'Comfortably Numb',
+    },
+    {
+      name: 'Huzaif Quazi',
+      quirk1: 'Calm',
+      quirk2: 'Smart',
+      img: webHuzaif,
+      quote: 'Izzat se baat kar',
+    },
+    {
+      name: 'Ayush Nayar',
+      quirk1: 'Knowlegable',
+      quirk2: 'Expert',
+      img: webAyush,
+      quote: 'Born to Overwhelm',
+    },
+    {
+      name: 'Shrikant Pandhekar',
+      quirk1: 'Innocent',
+      quirk2: 'Diligent',
+      img: webShrikant,
+      quote: 'Watashi ga Kitta!',
+    },
+    {
+      name: 'Riya Jain',
+      quirk1: 'Creative',
+      quirk2: 'Elegant',
+      img: webRiya,
+      quote: 'The best thing to hold on to in life is each other',
+    },
+  ],
+};
+
+export default teamData;
--- src/pages/committee.jsx
+++ src/pages/committee.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import DefaultLayout from 'layouts';
+import CommitteeComponent from 'components/CommitteeComponent';
+import TeamHeadings from 'assets/team/teamHeadings';
 
 const Committee = ({ match }) => {
-  return <DefaultLayout>{match.params.committee}</DefaultLayout>;
+  const { committee } = match.params;
+  return (
+    <DefaultLayout>
+      {TeamHeadings[committee]}
+      <CommitteeComponent Committee={committee} />
+    </DefaultLayout>
+  );
 };
 
 export default Committee;
