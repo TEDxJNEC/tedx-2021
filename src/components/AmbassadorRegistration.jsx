8bebdb58241ce76f999c5753da1b30c5050261aa
Shrikant Pandhekar <shrikantp9762@gmail.com>
2021-02-17 21:25:12 +0530

    WIP College Ambassador Registration Form
    - Component Created

---
+++ src/components/AmbassadorRegistration.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AmbassadorRegistration = () => {
+  return (
+    <div className="container">
+      <h1>Hello , This is College Ambassador Registration Form</h1>
+    </div>
+  );
+};
+
+export default AmbassadorRegistration;
