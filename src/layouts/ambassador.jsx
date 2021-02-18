3c247d157225d32857f66076c757113465b2767b
Pruthvi <pruthvishetty5656@gmail.com>
2021-02-19 02:38:42 +0530

    WIP: Dashboard cards added

--- src/layouts/ambassador.jsx
+++ src/layouts/ambassador.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
+import styled from 'styled-components';
 import Navbar from '../components/ambassdor/AmbassadorNavbar';
 import '../common/admin.scss';
 
+const AmbPageWrapper = styled.div`
+  display: flex;
+  background: #fff;
+  color: black;
+  min-height: 100%;
+`;
 const AmbassdorPageLayout = ({ children }) => {
   return (
-    <div className="admin-page">
+    <AmbPageWrapper>
       <Navbar />
       <div className="dashboard-wrapper">{children}</div>
-    </div>
+    </AmbPageWrapper>
   );
 };
 
--- src/pages/ambassador/ambassador-dashboard.jsx
+++ src/pages/ambassador/ambassador-dashboard.jsx
@@ -1,10 +1,22 @@
-import AmbassdorPageLayout from 'layouts/ambassador';
 import React from 'react';
+import styled from 'styled-components';
+import AmbassdorPageLayout from 'layouts/ambassador';
 
+const Dashboard = styled.div`
+  width: 100%;
+  min-height: 100%;
+`;
+const DashboardItem = styled.div`
+  border-radius: 8px;
+  flex-grow: 1;
+  border: 1px solid #dadce0;
+`;
 const ambassadorDashboard = () => {
   return (
     <AmbassdorPageLayout>
-      <div>hii</div>
+      <Dashboard>
+        <DashboardItem>Card1</DashboardItem>
+      </Dashboard>
     </AmbassdorPageLayout>
   );
 };
