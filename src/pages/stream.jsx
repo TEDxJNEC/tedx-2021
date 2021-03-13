b6bb6d7ba93499bbc57179f478d007fb20f9827e
Pruthvi Shetty <pruthvishetty5656@gmail.com>
2021-03-13 22:30:56 +0530

    fixes to controls

--- src/pages/stream.jsx
+++ src/pages/stream.jsx
@@ -9,18 +9,45 @@ const VideoPageWrapper = styled.div`
   align-items: center;
 `;
 
-const VideoWrapper = styled.div``;
-
-const Iframe = styled.iframe`
-  height: 390px;
-  width: 640px;
+const VideoWrapper = styled.div`
+  position: relative;
+  height: 100vh;
+  width: 100%;
 `;
 
+const Iframe = styled.iframe`
+  height: 100%;
+  width: 100%;
+`;
+const YTMask = styled.div`
+  position: absolute;
+  height: 40px;
+  width: 120px;
+  background: transparent;
+  right: 9px;
+  bottom: 13px;
+`;
+const WLShMask = styled.div`
+  position: absolute;
+  height: 60px;
+  width: 150px;
+  background: transparent;
+  right: 9px;
+  top: 13px;
+`;
+const AccWrapper = styled.div`
+  position: absolute;
+  height: 50px;
+  width: 200px;
+  background: transparent;
+  left: 9px;
+  top: 10px;
+`;
 const Stream = () => {
   useEffect(() => {
-    // document.addEventListener('contextmenu', (e) => {
-    //   e.preventDefault();
-    // });
+    document.addEventListener('contextmenu', (e) => {
+      e.preventDefault();
+    });
     document.querySelectorAll('button', (e) => {
       e.preventDefault();
     });
@@ -29,12 +56,15 @@ const Stream = () => {
     <VideoPageWrapper>
       <VideoWrapper>
         <Iframe
-          src="https://www.youtube.com/embed/sDw98aSq7Ls?rel=0&autoplay=1&modestbranding=1&showinfo=0&controls=0&autohide=2&disablekb=1"
+          src="https://www.youtube.com/embed/r3Hb0ZAmOwU?rel=0&autoplay=1&modestbranding=1&showinfo=0&controls=0&autohide=2&disablekb=1&version=3"
           frameBorder="0"
           allow="autoplay; encrypted-media"
           allowFullScreen
           title="video"
         />
+        <YTMask />
+        <WLShMask />
+        <AccWrapper />
       </VideoWrapper>
     </VideoPageWrapper>
   );
